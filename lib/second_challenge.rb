7f4193b1558550c7313877966a2ece72b3ec90d1
Beth Schofield <bethmschofield@gmail.com>
2018-02-26 04:06:36 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,14 +2,18 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
-      patriach: {}
-      matriach:{}
-      hero: {}
+      patriach: {},
+      matriach:{},
+      hero: {},
       hero_friends: []
       }, 
     :capulet => {
       #your key/value pairs here!
+      patriach: {},
+      matriach:{},
+      hero: {},
+      hero_friends: []
       }}
 
-  
+
 end
