0dff41768197e0628b68ff871b0323fe06b7d407
Beth Schofield <bethmschofield@gmail.com>
2018-02-26 04:11:01 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,11 +14,11 @@ def third_challenge
       :hero => {
         #your key/value pairs here
         name: "Romeo",
-        age: "15"
+        age: "15",
         status: "alive"
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         #your key/value pairs here
@@ -40,5 +40,5 @@ def third_challenge
    }
   }
 
-  
+
 end
