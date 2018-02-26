6ca868862c168992372fc1f2492808fae8e3f396
Beth Schofield <bethmschofield@gmail.com>
2018-02-26 04:04:43 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,9 +1,20 @@
+# def first_challenge
+#    epic_tragedy = {
+#      montague: {
+#      }
+# 
+#      capulet: {
+#        
+#      }
+#    }
+# end
+
 def first_challenge
    epic_tragedy = {
-     montague: {
+     :montague => {
      }
 
-     capulet: {
+     :capulet => {
        
      }
    }
